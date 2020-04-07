7f24b58138904aaa70b3c518f894836a0251cb5f
Hung Tran <tkhung2004@gmail.com>
2020-04-07 18:31:50 +0800

    debug 5

--- HW3_task1/agent/dqn.py
+++ HW3_task1/agent/dqn.py
@@ -365,7 +365,7 @@ def get_env():
     #                 LaneSpec(cars=2, speed_range=[-3, -1])
     #             ] }
 
-    config = {'observation_type': 'tensor', 'agent_speed_range': [-3, -1], 'width': 50,
+    config = {'observation_type': 'tensor', 'agent_speed_range': [-3, -1], 'stochasticity': 0.0, 'width': 50,
               'lanes': [LaneSpec(cars=7, speed_range=[-2, -1]),
                         LaneSpec(cars=8, speed_range=[-2, -1]),
                         LaneSpec(cars=6, speed_range=[-1, -1]),
