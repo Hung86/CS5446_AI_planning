1320af8fe924ec56b56aa650589fbde37539d83f
Hung Tran <tkhung2004@gmail.com>
2020-04-07 14:59:04 +0800

    debug 2

--- HW3_task2/agent/train_model.py
+++ HW3_task2/agent/train_model.py
@@ -1,5 +1,7 @@
-import models
-import dqn_evn
+from .models import *
+from .dqn_agent import *
+from .replay_buffer import *
+from .dqn_env import *
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
