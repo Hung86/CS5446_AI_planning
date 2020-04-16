89ba7c326d471383b6d0cd15c6b62c014f58ab41
Hung Tran <tkhung2004@gmail.com>
2020-04-16 12:59:04 +0800

    actor-critic

--- HW3_task2/agent/actor_critic.py
+++ HW3_task2/agent/actor_critic.py
@@ -36,7 +36,7 @@ class ActorCritic():
     def __init__(self, env, network_model):
         self.log_probs = None
         self.actor = network_model(env.observation_space.shape, env.action_space.n).to(device)
-        self.critic = network_model(env.observation_space.shape, env.action_space.n).to(device)
+        self.critic = network_model(env.observation_space.shape, 1).to(device)
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=learning_rate)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=learning_rate)
 
