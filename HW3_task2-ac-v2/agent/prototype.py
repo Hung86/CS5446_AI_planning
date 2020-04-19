1f5dcc8fe040f350352af4ec80d5441accdcb5d1
Hung Tran <tkhung2004@gmail.com>
2020-04-19 10:31:27 +0800

    lr=0.001 & gamma=0.98

--- HW3_task2-ac-v2/agent/prototype.py
+++ HW3_task2-ac-v2/agent/prototype.py
@@ -9,8 +9,8 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 model_path = os.path.join(script_path, 'model.pt')
 
 # Hyperparameters --- don't change, RL is very sensitive
-learning_rate = 0.0001
-gamma         = 0.99
+learning_rate = 0.001
+gamma         = 0.98
 buffer_limit  = 100000
 batch_size    = 32
 max_episodes  = 10000
