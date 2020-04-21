e05014790513b28b0e4dacdf5425daa830115333
Hung Tran <tkhung2004@gmail.com>
2020-04-21 23:42:25 +0800

    submit hw3 task1

--- HW3_Task1/agent/dqn.py
+++ HW3_Task1/agent/dqn.py
@@ -42,6 +42,9 @@ class ReplayBuffer():
         FILL ME : This function should initialize the replay buffer `self.buffer` with maximum size of `buffer_limit` (`int`).
                   len(self.buffer) should give the current size of the buffer `self.buffer`.
         '''
+        #dequeu to limit buffer and auto remove element when full
+        self.buffer = collections.deque(maxlen= buffer_limit)
+        #print("size",len(self.buffer))
         pass
     
     def push(self, transition):
@@ -55,6 +58,8 @@ class ReplayBuffer():
         Output:
             * None
         '''
+        #Dequeue will append elements from the right and remove elements from the left when full
+        self.buffer.append(transition)
         pass
     
     def sample(self, batch_size):
@@ -73,6 +78,25 @@ class ReplayBuffer():
                 * `dones`       (`torch.tensor` [batch_size, 1])
               All `torch.tensor` (except `actions`) should have a datatype `torch.float` and resides in torch device `device`.
         '''
+       
+        #random a batch of samples
+        rand_transitions = random.sample(self.buffer, batch_size)
+
+        #zip and unzip the elements together
+        sample_batch = Transition(*zip(*rand_transitions))
+
+        #Get the relevant elements of the batch
+        state_tensor = torch.tensor(sample_batch.state, dtype=torch.float32).to(device)
+        actions_tensor = torch.LongTensor(sample_batch.action).to(device)
+        rewards_tensor = torch.tensor(sample_batch.reward, dtype=torch.float32).to(device)
+        next_state_tensor = torch.tensor(sample_batch.next_state, dtype=torch.float32).to(device)
+        dones_tensor = torch.tensor(sample_batch.done, dtype=torch.float32).to(device)
+
+        
+        output_tuple =(state_tensor, actions_tensor, rewards_tensor, next_state_tensor, dones_tensor)
+    
+        return output_tuple
+
         pass
 
     def __len__(self):
@@ -122,6 +146,14 @@ class BaseAgent(Base):
         Output: action (`Action` or `int`): representing the action to be taken.
                 if action is of type `int`, it should be less than `self.num_actions`
         '''
+        #Get a random number and determine if agent should exploit or explore
+        rand_num = np.random.random()
+        if(rand_num < epsilon):#explore by choosing random action
+            output_action = np.random.randint(self.num_actions)
+        else:                   #exploit by choosing best action
+            output_actions = self.forward(state)
+            output_action = torch.argmax(output_actions).item()
+        return output_action
         pass
 
 class DQN(BaseAgent):
@@ -162,6 +194,23 @@ def compute_loss(model, target, states, actions, rewards, next_states, dones):
         * MSE Loss  : https://pytorch.org/docs/stable/nn.html#torch.nn.MSELoss
         * Huber Loss: https://pytorch.org/docs/stable/nn.html#torch.nn.SmoothL1Loss
     '''
+
+    indices = np.arange(len(rewards))
+    q_state_values = model(states).gather(1,actions)
+    
+    q_next_state_values = target(next_states).max(1)[0].detach()
+    
+    #Create mask based on terminal state
+    mask= dones.clone()
+    mask[dones==0.0] = 1.0
+    mask[dones> 0.0] = 0.0
+    
+    #If its terminal(done), then set as reward only
+    q_target_value = ((q_next_state_values*gamma)*mask[indices,[0]])+ rewards[indices,[0]]
+
+    theloss = F.smooth_l1_loss(q_state_values, q_target_value.unsqueeze(1))
+    
+    return theloss
     pass
 
 def optimize(model, target, memory, optimizer):
